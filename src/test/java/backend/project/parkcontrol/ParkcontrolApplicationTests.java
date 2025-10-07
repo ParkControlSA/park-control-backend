64578358ce8b66b7a376a0313b9d86e525815ffd
DiegoAvilaEstrada <de@t2mc.net>
2025-10-06 23:04:28 -0600

    init backend project

---
+++ src/test/java/backend/project/parkcontrol/ParkcontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.project.parkcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkcontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
